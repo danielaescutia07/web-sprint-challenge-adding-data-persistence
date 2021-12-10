7673e80068d5220f1558ec6294a48e70375c57d0
danielaescutia07 <danielaescutia@yahoo.com>
2021-12-10 12:56:08 -0600

    index, server and router are wired and catchAll endpoint is working

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
-// build your server here and require it from index.js
+const express = require('express');
+const projectsRouter = require('./project/router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+
+server.use('*', (req, res) => {
+    res.json('catch all endpoint is working!')
+})
+
+module.exports = server;
